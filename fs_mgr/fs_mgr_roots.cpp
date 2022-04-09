54235fec78d77716d3c10045657faf4781d7bc5b
afterallafk <shivamatiet2001@gmail.com>
2022-04-09 13:10:14 +0000

    Revert "fs_mgr: autodetect filesystem type"
    This reverts commit 9634b1d80f4d22228a202bb6915d4535ada39fad.

--- fs_mgr/fs_mgr_roots.cpp
+++ fs_mgr/fs_mgr_roots.cpp
@@ -37,7 +37,7 @@ FstabEntry* GetEntryForPath(Fstab* fstab, const std::string& path) {
     if (path.empty()) return nullptr;
     std::string str(path);
     while (true) {
-        auto entry = GetEntryForMountPointTryDetectFs(fstab, str);
+        auto entry = GetEntryForMountPoint(fstab, str);
         if (entry != nullptr) return entry;
         if (str == "/") break;
         auto slash = str.find_last_of('/');
